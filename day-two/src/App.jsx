61961e79cdb41b50aba53b5685f0f6283c097d9e
yoni-tad <yonitad2404@gmail.com>
2025-03-20 12:00:30 +0300

    demo video

--- day-two/src/App.jsx
+++ day-two/src/App.jsx
@@ -1,22 +1,30 @@
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import TaskList from "./components/TaskList";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 
 export default function App() {
-  const [userData, setUserData] = useState([
-    {
-      id: uuid(),
-      task: "Follow 7 day react challenge",
-      isComplete: false,
-    },
-  ]);
+  const [userData, setUserData] = useState(
+    localStorage.getItem("Task")
+      ? JSON.parse(localStorage.getItem("Task"))
+      : [
+          {
+            id: uuid(),
+            task: "Follow 7 day react challenge",
+            isComplete: false,
+          },
+        ]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("Task", JSON.stringify(userData));
+  }, [userData]);
 
   const inputRef = useRef();
 
   function addTask() {
-    const task = inputRef.current.value;
+    const task = inputRef.current.value.trim();
     if (task) {
       setUserData((prevData) => {
         return [
